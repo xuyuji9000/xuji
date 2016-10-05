11ebf071a2c7333a6ad4fba47c16e328567047b1
yogiman <xuyuji9000@163.com>
2016-10-05 23:41:41 +0800

    update picture manage.

--- app/Picture.php
+++ app/Picture.php
@@ -16,19 +16,19 @@ class Picture extends Model
         'org'
     ];
 
+
+
     /**
 	 *	$file_name : 表单中file的name属性
 	 *	返回值：数组   array("org"=>org/1/b9/e73/64e7/4bd979d19b34918fc277f8.jpg, "id=>"1")
 	 */
-	public function upload_file($souce_file)
+	public function uploadFile($souce_file)
     {
     	$upload=new UploadFile($souce_file);
-    	// $data = array();
 
     	$rr["org"]=$upload->copy_org();
 		if($rr["org"]["result"])
 		{
-			// $data["org"]=$rr["org"]["img_url"];
 			$this->org=$rr["org"]["img_url"];
 		}
 		$upload->destroy();
@@ -36,7 +36,10 @@ class Picture extends Model
 		return $this->id?$this->id:false;
     }
 
-    private function get_img_base_path()
+    /**
+     * @return string
+     */
+    private function getImageBasePath()
     {
     	return '/'.env("IMAGE_PATH");
     }
@@ -45,12 +48,26 @@ class Picture extends Model
 	 *	获取图片链接
 	 *	返回值: 字符串 图片链接 "http://xuji.yogiman.cn/../../../test.jpg"
 	 */
-    public function get_img_url($org_path='') {
+    public function getImageURL($org_path='') {
     	$org_path = $org_path?$org_path:$this->org;
-    	return URL::to('/').$this->get_img_base_path().$org_path;
+    	return URL::to('/').$this->getImageBasePath().$org_path;
     }
 
-    public function get_img_local(){
-    	return public_path().'/'.env("IMAGE_PATH").$this->org;
+    /**
+     * @return string
+     */
+    public function getLocalImage(){
+    	return public_path().$this->getImageBasePath().$this->org;
+    }
+
+    /**
+     * @param $id
+     * @return bool
+     */
+    function getElementById($id){
+        if(!($result = Picture::find($id)))
+            return false;
+        $result['org'] = $this->getImageBasePath().$result['org'];
+        return $result;
     }
 }
