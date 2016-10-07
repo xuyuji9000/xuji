030e29e84d3ff8fafa3c9cc53d28241451c9c1e8
yogiman <xuyuji9000@163.com>
2016-10-08 03:40:30 +0800

    goods list

---
+++ database/migrations/2016_10_05_135900_create_goods_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('good_picture');
+            $table->integer('qr_code');
+            $table->integer('price');
+            $table->string('description', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('goods');
+    }
+}
---
+++ resources/views/goods/list.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="page-header">
+                <h1>
+                    <span>Goods Gallery</span>
+                    <button type="button" class="btn btn-default btn-lg pull-right">Add Goods</button>
+                </h1>
+            </div>
+
+            @foreach ($goods as $good)
+                <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                    <a class="thumbnail" href="#">
+                        <img class="img-responsive" src="{{ App\Picture::getPathById($good['good_picture']) }}" alt="{{ $good['title'] }}" title="{{ $good['title'] }}">
+                    </a>
+                </div>
+            @endforeach
+        </div>
+        {{ $goods->links() }}
+    </div>
+@endsection
