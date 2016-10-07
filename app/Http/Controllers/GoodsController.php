50324c5fc8c870d535dc58bab475007e36be6621
yogiman <xuyuji9000@163.com>
2016-10-08 03:41:27 +0800

    goods list III

---
+++ app/Http/Controllers/GoodsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Good;
+
+class GoodsController extends Controller
+{
+    public function list(){
+        $goods = Good::paginate(12);
+
+
+        return view("goods.list", ['goods'=>$goods]);
+    }
+}
